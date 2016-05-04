7bdd32d1ea98052aa1b97d5479fa6db2cc9abafa
Fer Uría <fauria@gmail.com>
2016-05-04 18:01:40 +0200

    Update README and picture assets.

--- app_code/public/javascripts/smart-closet.js
+++ app_code/public/javascripts/smart-closet.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://10.0.214.1:3000');
+var socket = io.connect('http://127.0.0.1:3000');
 $(document).ready(function(){
 	console.log('Document ready!');	
 	socket.on('clothes', function (clothes) {
